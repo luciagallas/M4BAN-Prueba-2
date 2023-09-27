1fd24f91adae4383ae160878d3659dcb62e00b7e
Luli <lugallas@gmail.com>
2023-09-27 08:26:42 -0300

    [RED] new calculator test

---
+++ BusinessLogic.Test/CalculatorTest.cs
@@ -0,0 +1,15 @@
+namespace BusinessLogic.Test;
+
+[TestClass]
+public class CalculatorTest
+{
+    [TestMethod]
+    public void newCalculatorTest()
+    {
+        var calculator = new CostCalculator();
+        var baldosas = new List<Baldosa>();
+        var precioPorMetroCuadrado = 10.0;
+        var costoTotal = calculator.CalcularCostoTotal(baldosas, precioPorMetroCuadrado);
+        Assert.AreEqual(0.0, costoTotal);
+    }
+}
